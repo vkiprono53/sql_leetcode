1d3a8aa9820e621b6968159e20557076ab3539b9
Vkiprono53 <kipronovincent53@gmail.com>
2024-03-22 11:20:14 +0300

    1378 - replace employee id

---
+++ 1378. replace_employee_id_unique_identifier.sql
@@ -0,0 +1,39 @@
+/**
+ * 
+ * LEETCODE 1378. Replace Employee ID With The Unique Identifier
+ * 
+ * 
+ * Table: Employees
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains the id and the name of an employee in a company.
+
+
+Table: EmployeeUNI
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| unique_id     | int     |
++---------------+---------+
+(id, unique_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id and the corresponding unique id of an employee in the company.
+
+
+Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+
+Return the result table in any order.
+
+The result format is in the following example.
+ * /
+ 
+ SELECT u.unique_id, e.name
+FROM Employees e  LEFT JOIN EmployeeUNI u
+ON u.id = e.id;
